2ac4a75cb91dda58ec95ed15c7dcdce4d4eaeee2
Erlend Lind Madsen <erlendf80@gmail.com>
2022-03-14 23:31:00 +0100

    test: add a few basic tests for MuState

--- test/mu_state_test.dart
+++ test/mu_state_test.dart
@@ -3,10 +3,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mu_state/mu_state.dart';
 
 void main() {
-  test('adds one to input values', () {
-    //final calculator = Calculator();
-    //expect(calculator.addOne(2), 3);
-    //expect(calculator.addOne(-7), -6);
-    //expect(calculator.addOne(0), 1);
+  test('create MuState with MuEvent.loading', () {
+    final muState = MuState<int>(const MuEvent.loading());
+    expect(muState.value.loading, true);
+  });
+
+  test('create MuState with MuEvent.loading and data is null', () {
+    final muState = MuState<int>(const MuEvent.loading());
+    expect(muState.value.data, null);
+  });
+
+  test('create MuState with MuEvent.loading and error is null', () {
+    final muState = MuState<int>(const MuEvent.loading());
+    expect(muState.value.error, null);
+  });
+
+  test('create MuState with MuEvent.loading(false)', () {
+    final muState = MuState<int>(const MuEvent.loading(false));
+    expect(muState.value.loading, false);
+  });
+
+  test('create MuState with MuEvent.error', () {
+    final muState = MuState<int>(MuEvent.error(AssertionError('Error')));
+    expect(muState.value.error, isA<AssertionError>());
   });
 }
