857f4b23d80ce37f658c42eb5eb12d027a0d4d14
Erlend Lind Madsen <erlendf80@gmail.com>
2022-03-27 21:44:49 +0200

    mu_state_test: rename muState -> state

--- test/mu_state_test.dart
+++ test/mu_state_test.dart
@@ -4,25 +4,25 @@ import 'package:mu_state/mu_state.dart';
 
 void main() {
   test('create MuState with MuEvent.data and other states', () {
-    final muState = MuState(const MuEvent.data('Data'));
-    expect(muState.value.data, 'Data');
-    expect(muState.value.hasData, true);
-    expect(muState.value.loading, false);
-    expect(muState.value.hasError, false);
+    final state = MuState(const MuEvent.data('Data'));
+    expect(state.value.data, 'Data');
+    expect(state.value.hasData, true);
+    expect(state.value.loading, false);
+    expect(state.value.hasError, false);
   });
 
   test('create MuEvent.loading and check loading is true and other states', () {
-    final muState = MuState(const MuEvent.loading());
-    expect(muState.value.loading, true);
-    expect(muState.value.hasData, false);
-    expect(muState.value.hasError, false);
+    final state = MuState(const MuEvent.loading());
+    expect(state.value.loading, true);
+    expect(state.value.hasData, false);
+    expect(state.value.hasError, false);
   });
 
   test('create MuEvent.error and assert it has error and other states', () {
-    final muState = MuState(MuEvent.error(AssertionError('Error')));
-    expect(muState.value.error, isA<AssertionError>());
-    expect(muState.value.hasError, true);
-    expect(muState.value.hasData, false);
-    expect(muState.value.loading, false);
+    final state = MuState(MuEvent.error(AssertionError('Error')));
+    expect(state.value.error, isA<AssertionError>());
+    expect(state.value.hasError, true);
+    expect(state.value.hasData, false);
+    expect(state.value.loading, false);
   });
 }
