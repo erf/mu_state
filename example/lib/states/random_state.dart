a64a187c9c1fc1d492dbab8af4370488a52de263
Erlend Lind Madsen <erlendf80@gmail.com>
2022-03-27 12:29:33 +0200

    random_state: use MuEvent.data

--- example/lib/states/random_state.dart
+++ example/lib/states/random_state.dart
@@ -5,7 +5,7 @@ import 'package:mu_state/mu_state.dart';
 class AutoCounterState extends MuState<int> {
   AutoCounterState(MuEvent<int> initValue) : super(initValue) {
     Timer.periodic(const Duration(milliseconds: 1000), (timer) {
-      value = MuEvent(data: value.data! + 1);
+      value = MuEvent.data(value.data! + 1);
     });
   }
 }
